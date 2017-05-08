98012076f47b44b027054508ad199c697093822a
Esra Petursson <esrapetursson@Esras-MacBook-Pro.local>
2017-05-08 14:23:23 -0400

    Blog app for Ruby on Rails

---
+++ Blog/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#  root 'welcome#index'
+#end
+
+#Rails.application.routes.draw do
+
+#  resources :articles
+#  root 'welcome#index'
+#end
+
+resources :articles do
+  resources :comments
+end
+end
